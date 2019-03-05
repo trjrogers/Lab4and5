f20f78ce54a9422b2e2f37309cb14088de01a2ed
Trevor R <rogerst@my.lanecc.edu>
2019-03-05 13:44:34 -0800

    finished 4a

--- CustomerMaintenanceStart/CustomerMaintenanceClasses/Customer.cs
+++ CustomerMaintenanceStart/CustomerMaintenanceClasses/Customer.cs
@@ -8,6 +8,60 @@ namespace CustomerMaintenanceClasses
 {
     public class Customer
     {
+        private string firstName;
+        private string lastName;
+        private string email;
 
+        public Customer() { }
+
+        public Customer(string first, string last, string em)
+        {
+            firstName = first;
+            lastName = last;
+            email = em;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+
+        public string GetDisplayText()
+        {
+            string info;
+            info = firstName + " " + lastName + ", " + email;
+            return info;
+        }
     }
 }
